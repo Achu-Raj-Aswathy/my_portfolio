9865ac6b35a3434b06c7f876af122da58f1b54e0
Achu-Raj-Aswathy <achurajaswathy@gmail.com>
2024-03-07 01:03:27 +0530

    my portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -11,13 +11,13 @@ function About() {
                 </div>
                 <div className='container'>
                     <Row className='flex-md-row-reverse'>
-                        <Col lg={6} xs={12} md={6} className='mt-2 pt-5 p-4'>
+                        <Col lg={6} xs={12} md={6} className='mt-4'>
                             <p className='d-flex text-justify'>Hello! I'm Aswathy Raj, a passionate and driven Full Stack Developer with a keen interest in creating innovative solutions and enhancing user experiences.
                                 I recently graduated with a degree in Computer Science and am eager to apply my skills and knowledge in a professional setting.
                                 My journey into the world of programming began with a fascination for technology and a desire to solve real-world problems through code.
                                 I thrive in dynamic environments where I can collaborate with others to tackle challenges and continuously learn and grow.</p>
                         </Col>
-                        <Col lg={6} xs={12} md={6} className='pt-5 p-4'>
+                        <Col lg={6} xs={12} md={6} className='mt-4'>
                             <h4 className='d-flex align-items-center justify-content-center mt-3'><b style={{ fontSize: "35px" }}>Personal Information</b></h4>
                             <br />
                             <p>
